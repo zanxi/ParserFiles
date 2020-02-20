ee2176da3f63f47b502de9a341ceb2be6352cadf
Zanxi <consultantweb25@gmail.com>
2020-02-20 13:29:55 +0600

    Edit

---
+++ _release___/Stl_lib_Threads_05_readfile__.bat
@@ -0,0 +1,4 @@
+Stl_lib_Threads_05_readfile_from_directory.exe D:\\_V2020_\\_Rabota_2020__\\_elveesneotek_com__\\cat\\
+rem Stl_lib_Threads_05_readfile_from_directory.exe D:\\_Soft_delll__\\cpu-z-1261_
+rem Stl_lib_Threads_05_readfile_from_directory.exe  D:\\_Books__
+rem Stl_lib_Threads_05_readfile_from_directory.exe  D:\\_2016__\\_ws_9_\\_________
